704e8f487202dd1b22577edb6eb1788a39de9084
YannSG <95168319+YannSG@users.noreply.github.com>
2022-11-09 12:16:05 +0100

    Remove unused imports

--- src/test/java/fr/ecoleql/AppTest.java
+++ src/test/java/fr/ecoleql/AppTest.java
@@ -2,8 +2,6 @@ package fr.ecoleql;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-
 import java.io.FileInputStream;
 import java.time.Duration;
 import java.util.ArrayList;
